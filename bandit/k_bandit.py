c7d82d870199784e4caf86c154890e5aa8043e1e
akurmustafa <akurmustafa@gmail.com>
2021-01-02 11:07:28 +0300

    bandit algorithm and simulation added

---
+++ bandit/k_bandit.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.matlib
+# parameters to set #######################
+n_bandit = 10
+variance_reward_init = 1
+variance_reward = 1
+mean_reward_init = 0
+n_step = 1000
+n_sim = 2000
+print_every = 20000
+print_every_sim = 10
+stationary_mode = False
+alpha = None if stationary_mode else 0.1
+variance_nonstationary = 0  # 0.01
+mean_nonstationary = 0
+epsilons = [0, 0.1]
+Q_inits = [0, 5]
+parameters = [{'Q':0, 'eps':0}, {'Q':0, 'eps':0.01}, {'Q':0, 'eps':0.1}]
+# parameters to set #######################
+fig ,ax = plt.subplots()
+fig2, ax2 = plt.subplots()
+# for eps, eps_idx in zip(epsilons, range(len(epsilons))):
+#     for action_value_init, action_value_init_idx in zip(Q_inits, range(len(Q_inits))):
+for cur_params, cur_params_idx in zip(parameters, range(len(parameters))):
+    eps = cur_params['eps']
+    action_value_init = cur_params['Q']
+    average_reward = np.zeros((n_sim, n_step))
+    actions_counts = np.zeros((n_sim, n_bandit), dtype=np.int32)
+    action_counts_ratio = np.zeros((n_sim, n_step))
+    action_counts_true = np.zeros((n_sim, n_step))
+    gt_actions = np.zeros((n_sim, 1), dtype=np.int32)
+    for sim_idx in range(n_sim):
+        reward_means = np.random.randn(1, n_bandit)*variance_reward_init+mean_reward_init
+        gt_actions[sim_idx, 0] = np.argmax(reward_means)
+        # action_values_hist = np.matlib.repmat(reward_means, n_step+1, 1)
+        action_values_hist = np.zeros((n_step+1, n_bandit))
+        action_values_init = np.ones((1, n_bandit))*action_value_init
+        action_values_hist[0, :] = action_values_init
+        for i in range(n_step):
+            if np.random.rand()>eps:
+                action = np.argmax(action_values_hist[i,:])
+            else:
+                action = np.random.randint(0, n_bandit)
+            cur_reward = np.random.randn()*variance_reward+reward_means[0, action]
+            reward_means+=np.random.randn(1, n_bandit)*variance_nonstationary+mean_nonstationary
+            average_reward[sim_idx, i] = cur_reward
+            actions_counts[sim_idx, action]+=1
+            action_counts_ratio[sim_idx, i] = actions_counts[sim_idx, gt_actions[sim_idx, 0]]/(i+1)
+            if action == gt_actions[sim_idx, 0]:
+                action_counts_true[sim_idx, i] = 1
+            action_values_hist[i+1, :] = action_values_hist[i, :]
+            if stationary_mode:
+                alpha_coeff = 1.0/(actions_counts[sim_idx, action])
+            else:
+                alpha_coeff = alpha
+            action_values_hist[i+1, action] = action_values_hist[i, action]+alpha_coeff*(cur_reward-action_values_hist[i, action])
+            if (i+1) % print_every == 0:
+                print('step progress: {}/{}, {}/{}'.format(i+1, n_step, sim_idx+1, n_sim))
+        if (sim_idx+1)%print_every_sim == 0:
+            print('sim progress: {}/{}, Q: {}, eps: {}, param progress: {}/{}'.format(sim_idx+1, n_sim, 
+                    action_value_init, eps, cur_params_idx+1, len(parameters)))
+    ax.plot(np.mean(average_reward, axis=0), label='Q1: {}, eps: {}'.format(action_value_init, eps))
+    # ax2.plot(np.sum(actions_counts[np.arange(n_sim), gt_actions.flatten().astype(int)])/(n_sim*n_step), label='eps: '+str(eps))
+    # ax2.plot(np.mean(action_counts_ratio, axis=0)*100, label='Q1: {}, eps: {}'.format(action_value_init, eps))
+    ax2.plot(np.sum(action_counts_true, axis=0)/n_sim*100, label='Q1: {}, eps: {}'.format(action_value_init, eps))
+    print('param progress: {}/{}'.format(cur_params_idx+1, len(parameters)))
+ax.legend()
+ax2.legend()
+plt.show()
+
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Project Title
+
+Implementations of Simple RL (Reinforcement Learning) Algortithms in Python
+
+### Prerequisites
+
+You need python >=3, numpy, scipy, matplotlib libraries to be able to run scripts
+
+## Authors
+
+* **Mustafa Akur** - *Initial work* - [akurmustafa](https://github.com/akurmustafa)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* I have benefited a lot from [David Silver's lectures on RL](https://www.youtube.com/watch?v=2pWv7GOvuf0&list=PLqYmG7hTraZDM-OYHWgPebj2MfCFzFObQ) and the book "[Reinforcement Learning: An Introduction](https://www.amazon.com/Reinforcement-Learning-Introduction-Adaptive-Computation/dp/0262039249)" by Richard S. Sutton, Andrew G. Barto.
